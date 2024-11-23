f8012e58cfce58b1ba9e71f6495c338af51013bd
avimallik <armavicse@gmail.com>
2024-11-23 17:17:56 +0600

    Initial Commit

---
+++ app/src/main/java/com/armavi_bsd/userrobotisp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.armavi_bsd.userrobotisp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
